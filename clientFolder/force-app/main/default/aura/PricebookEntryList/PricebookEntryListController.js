d3a9eef6a5041d3a20240b9b827428b8b2d89be9
Blake Prescott <blakewillprescott@gmail.com>
2021-08-16 11:35:57 -0500

    created aura components for the pricebook page migration

---
+++ clientFolder/force-app/main/default/aura/PricebookEntryList/PricebookEntryListController.js
@@ -0,0 +1,51 @@
+({
+    init: function (component, event, helper) {
+        component.set("v.limit", "10");
+        component.set("v.offset", "0");
+        component.set('v.mycolumns', [            
+            { label: 'Product Name', fieldName: 'ProductUrl', type: 'url', sortable: 'true', typeAttributes: {
+                label: { 
+                    fieldName: 'ProductName' 
+                },
+                target : '_blank'
+            }},
+            { label: 'Unit Price', fieldName: 'UnitPrice', type: 'currency', sortable: 'true'},
+            { label: 'Product Type', fieldName: 'ProductType', type: 'text', sortable: 'true'},
+            { label: 'Pricebook', fieldName: 'PricebookUrl', type: 'url', sortable: 'true', typeAttributes: {
+                label: { 
+                    fieldName: 'PricebookName' 
+                },
+                target : '_blank'
+            }}
+            
+        ]);
+        helper.getData(component);
+    },
+    
+    updateColumnSorting: function (component, event, helper) {
+        var fieldName = event.getParam('fieldName');
+        var sortDirection = event.getParam('sortDirection');
+        // assign the latest attribute with the sorted column fieldName and sorted direction
+        component.set("v.sortedBy", fieldName);
+        component.set("v.sortDirection", sortDirection);
+        helper.sortData(component, fieldName, sortDirection);
+    },
+    
+    handleNext : function(component, event, helper) { 
+        var pageNumber = component.get("v.pageNumber");
+        var os = component.get("v.offset");
+        component.set("v.pageNumber", pageNumber+1);
+        component.set("v.offset", os+10);
+        helper.getData(component);
+    },
+     
+    handlePrev : function(component, event, helper) {        
+        var pageNumber = component.get("v.pageNumber");
+        var os = component.get("v.offset");
+        component.set("v.pageNumber", pageNumber-1);
+        component.set("v.offset", os-10);
+        helper.getData(component);
+    }
+    
+    
+})
---
+++ clientFolder/force-app/main/default/aura/PricebookEntryList/PricebookEntryListHelper.js
@@ -0,0 +1,66 @@
+({
+    getData : function(component) {
+        let qLimit = component.get("v.limit");
+        let os = component.get("v.offset");
+        let action = component.get("c.getPriceBookEntryList");
+        action.setParams({queryLimit : qLimit, offset : os});
+        action.setCallback(this, $A.getCallback(function (response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                console.log(response.getReturnValue());
+                if(response.getReturnValue().length < component.get("v.limit")){
+                    component.set("v.isLastPage", true);
+                } else{
+                    component.set("v.isLastPage", false);
+                }
+                component.set("v.dataSize", response.getReturnValue().length);
+                let wrappedEntry = this.getWrapper(response.getReturnValue());
+                component.set('v.mydata', wrappedEntry);
+            } else if (state === "ERROR") {
+                var errors = response.getError();
+                console.error(errors);
+            }
+        }));
+        $A.enqueueAction(action);
+    },
+    
+    getWrapper : function(results){
+        console.log(results);
+        
+        let list = [];
+        let map = {};
+        let baseUrl = 'https://'+location.host+'/';
+        for (let i = 0; i < results.length; i++) {
+            map = {};
+            map.UnitPrice = results[i].UnitPrice;
+            map.ProductName = results[i].Product2.Name;
+            map.ProductType = results[i].Product2.RecordType.Name;
+            map.PricebookName = results[i].Pricebook2.Name;
+            map.ProductId = results[i].Product2Id;
+            map.ProductUrl = baseUrl + results[i].Product2Id;
+            map.PricebookUrl = baseUrl + results[i].Pricebook2Id;
+            list.push(map);
+        }
+        console.log(map);
+        return list;
+        
+    },
+    
+    sortData: function (component, fieldName, sortDirection) {
+        var data = component.get("v.mydata");
+        var reverse = sortDirection !== 'asc';
+        //sorts the rows based on the column header that's clicked
+        data.sort(this.sortBy(fieldName, reverse))
+        component.set("v.mydata", data);
+    },
+    sortBy: function (field, reverse, primer) {
+        var key = primer ?
+            function(x) {return primer(x[field])} :
+            function(x) {return x[field]};
+        //checks if the two rows should switch places
+        reverse = !reverse ? 1 : -1;
+        return function (a, b) {
+            return a = key(a), b = key(b), reverse * ((a > b) - (b > a));
+        }
+    }
+})
